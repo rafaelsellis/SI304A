bc394fd1a104dce8a9e7719cbb72413e3066c342
rafaelsellis <rafaelsellis@gmail.com>
2016-05-16 00:39:50 -0300

    Add files via upload

--- Quiz_1.c
+++ Quiz_1.c
@@ -7,12 +7,12 @@ int main () {
     void Selecao_A(void);
     void Selecao_B(void);
 
-    printf("\n\n\nOI! SEJA BEM-VINDO AO NOSSO QUIZ DE MITOLOGIA! RESPONDA O TESTE AGORA E DESCUBRA QUAL HEROI DA MITOLOGIA EH VOCE!!!\n\n");
+    printf("\n\nOI! SEJA BEM-VINDO AO NOSSO QUIZ DE MITOLOGIA! RESPONDA O TESTE AGORA E DESCUBRA QUAL HEROI DA MITOLOGIA EH VOCE!!!\n\n");
     printf("PARA CADA PERGUNTA RESPONDA APENAS SIM OU NAO. Para inicar o QUIZ, aperte ENTER. \n");
     V = getchar();
     fflush(stdin);
 
-    printf("Voce eh mais um improvisador natural do que um planejador organizado? \n(1)SIM. | (2)NAO.\n");
+    printf("\nVOCE EH MAIS UM IMPROVISADOR NATURAL DO QUE UM PLANEJADOR ORGANIZADO? \n(1)SIM. | (2)NAO.\n");
     scanf("%d", &R);
         switch (R) {
         	case 1: {
@@ -26,22 +26,7 @@ int main () {
                     break;     }
         fflush(stdin);
 
-    printf("As suas viagens sao sempre bem planejadas? \n(1)SIM. | (2)NAO.\n");
-    scanf("%d", &R);
-        switch (R) {
-        	case 1: {
-                	GA=GA+2;
-                    R = 0; }
-                    break;
-        	case 2:
-                	{
-                	GB=GB+2;
-                    R = 0; }
-                    break;     }
-        fflush(stdin);
-
-
-    printf("Quando se trata de um trabalho em grupo, ser eficiente e mais importante do que ser colaborativo? \n(1)SIM. | (2)NAO.\n");
+    printf("\nAS SUAS VIAGENS SAO SEMPRE BEM PLANEJADAS? \n(1)SIM. | (2)NAO.\n");
     scanf("%d", &R);
         switch (R) {
         	case 1: {
@@ -56,7 +41,22 @@ int main () {
         fflush(stdin);
 
 
-    printf("Ficar de boa em casa para curtir seu seriado ou video game favorito, eh mais interessante do que um evento social com a galera? \n(1)SIM. | (2)NAO.\n");
+    printf("\nQUANDO SE TRATA DE UM TRABALHO EM GRUPO, SER EFICIENTE E MAIS IMPORTANTE DO QUE SER COLABORATIVO? \n(1)SIM. | (2)NAO.\n");
+    scanf("%d", &R);
+        switch (R) {
+        	case 1: {
+                	GA=GA+2;
+                    R = 0; }
+                    break;
+        	case 2:
+                	{
+                	GB=GB+2;
+                    R = 0; }
+                    break;     }
+        fflush(stdin);
+
+
+    printf("\nFICAR DE BOA EM CASA PARA CURTIR SEU SERIADO OU VIDEO GAME FAVORITO, EH MAIS INTERESSANTE DO QUE UM EVENTO SOCIAL? \n(1)SIM. | (2)NAO.\n");
     scanf("%d", &R);
         switch (R) {
         	case 1: {
@@ -70,7 +70,6 @@ int main () {
                     break;     }
         fflush(stdin);
 
-            // printf("GA= %i GB=%i", GA, GB);
 
     if  (GA > GB)
         Selecao_A();
@@ -81,7 +80,7 @@ int main () {
 }
 
 void Selecao_A(void) {
-    printf("Voce se sai melhor trabalhando sozinho do que trabalhando em equipe? \n(1)SIM. | (2)NAO.\n");
+    printf("\nVOCE SE SAI MELHOR TRABALHANDO SOZINHO DO QUE TRABALHANDO EM EQUIPE? \n(1)SIM. | (2)NAO.\n");
     scanf("%d", &R);
         switch (R) {
         	case 1: {
@@ -97,32 +96,32 @@ void Selecao_A(void) {
 
 
     if (LOKIZEUS==1){
-    printf("Voce prefere improvisar do que gastar tempo criando um plano detalhado? \n(1)SIM. | (2)NAO.\n");
+    printf("VOCE PREFERE IMPROVISAR DO QUE GASTAR TEMPO CRIANDO UM PLANO DETALHADO? \n(1)SIM. | (2)NAO.\n");
     scanf("%d", &R);
         switch (R) {
         	case 1: {
-                	printf ("VOCE EH ZEUS!!!/n/n");
+                	printf ("\n\n******************************\n       VOCE EH ZEUS!!!\n\n******************************\n\n");
                     R = 0; }
                     break;
         	case 2:
                 	{
-                	printf ("VOCE EH LOKI!!!/n/n");
+                	printf ("\n\n******************************\n       VOCE EH LOKI!!!\n\n******************************\n\n");
                     R = 0; }
                     break;         }
     fflush(stdin);  }
 
 
     if (ODINHORUS==1){
-    printf("Voce acha que uma decisao logica eh sempre o melhor, mesmo magoando algumas pessoas? \n(1)SIM. | (2)NAO.\n");
+    printf("\nVOCE ACHA QUE UMA DECISAO LOGICA EH SEMPRE O MELHOR, MESMO MAGOANDO ALGUMAS PESSOAS? \n(1)SIM. | (2)NAO.\n");
     scanf("%d", &R);
         switch (R) {
         	case 1: {
-                	printf ("VOCE EH ODIN!!!/n/n");
+                	printf ("\n\n******************************\n       VOCE EH ODIN!!!\n\n******************************\n\n");
                     R = 0; }
                     break;
         	case 2:
                 	{
-                	printf ("VOCE EH HORUS!!!/n/n");
+                	printf ("\n\n******************************\n       VOCE EH HORUS!!!\n\n******************************\n\n");
                     R = 0; }
                     break;         }
     fflush(stdin);  }
@@ -130,7 +129,7 @@ void Selecao_A(void) {
 
 
 void Selecao_B(void) {
-    printf("Eh facil deixar voce irritado? \n(1)SIM. | (2)NAO.\n");
+    printf("\nEH FACIL DEIXAR VOCE IRRITADO?\n(1)SIM. | (2)NAO.\n");
     scanf("%d", &R);
         switch (R) {
         	case 1: {
@@ -145,21 +144,21 @@ void Selecao_B(void) {
     fflush(stdin);
 
 
-    printf("Voce tem facilidade em se apresentar para as pessoas? \n(1)SIM. | (2)NAO.\n");
+    printf("VOCE TEM FACILIDADE EM SE APRESENTAR PARA AS PESSOAS?\n(1)SIM. | (2)NAO.\n");
     scanf("%d", &R);
         switch (R) {
         	case 1: {
                 	if (POSEIDONSETH == 1)
-                    printf ("VOCE EH SETH!!!/n/n");
+                    printf ("\n\n******************************\n       VOCE EH SETH!!!\n\n******************************\n\n");
                     if (THOROSIRIS == 1)
-                    printf ("VOCE EH THOR!!!/n/n");            }
+                    printf ("\n\n******************************\n       VOCE EH THOR!!!\n\n******************************\n\n");            }
                     break;
         	case 2:
                 	{
                 	if (POSEIDONSETH == 1)
-                    printf ("VOCE EH POSEIDON!!!/n/n");
+                    printf ("\n\n******************************\n       VOCE EH POSEIDON!!!\n\n******************************\n\n");
                     if (THOROSIRIS == 1)
-                    printf ("VOCE EH OSIRIS!!!/n/n");          }
+                    printf ("\n\n******************************\n       VOCE EH OSIRIS!!!\n\n******************************\n\n");          }
                     break;         }
     fflush(stdin);
 }
